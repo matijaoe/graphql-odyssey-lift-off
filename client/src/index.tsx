7919ab7b3c311bf724220d7e37923ff701763512
Matija Osrečki <github@matijao.com>
2024-05-24 19:29:26 +0200

    setup codegen

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import GlobalStyles from './styles';
-import Pages from './pages';
+import GlobalStyles from './styles'
+import Pages from './pages'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
-const root = createRoot(document.getElementById('root')!);
+const client = new ApolloClient({
+	// options go here
+	uri: 'http://localhost:4000',
+	cache: new InMemoryCache(),
+})
+
+const root = createRoot(document.getElementById('root')!)
 
 root.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<GlobalStyles />
+			<Pages />
+		</ApolloProvider>
+	</React.StrictMode>
+)

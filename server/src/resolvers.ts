1335e97ef3e1328c22fc67736635b7de1449b7f3
Matija Osrečki <github@matijao.com>
2024-06-25 21:24:26 +0200

    define track and module resvolers

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -5,13 +5,19 @@ export const resolvers: Resolvers = {
 	Query: {
 		// returns an array of Tracks that will be used to populate
 		// the homepage grid of our web client
-		tracksForHome: (_parent, _args, { dataSources }) => {
+		tracksForHome: (_, __, { dataSources }) => {
 			return dataSources.trackAPI.getTracksForHome()
+		},
+		track: (_, { id }, { dataSources }) => {
+			return dataSources.trackAPI.getTrack(id)
 		},
 	},
 	Track: {
-		author: ({ authorId }, _args, { dataSources }) => {
+		author: ({ authorId }, _, { dataSources }) => {
 			return dataSources.trackAPI.getAuthor(authorId)
+		},
+		modules: ({ id: trackId }, _, { dataSources }) => {
+			return dataSources.trackAPI.getTrackModules(trackId)
 		},
 	},
 }
